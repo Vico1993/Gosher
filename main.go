5b0bea3b263e1077bc6742fdb02c7771f6269e76
Victor Piolin <victor.piolin@gmail.com>
2018-05-03 19:01:50 -0700

    Works

--- main.go
+++ main.go
@@ -9,27 +9,15 @@ import (
 )
 
 type message struct {
-	id   string `json:"id"`
-	body string `json:"body"`
+	ID   int    `json:"id"`
+	Body string `json:"body"`
 }
 
+// Message Container
+var messages = make([]map[string]interface{}, 0)
+
 func main() {
-
-	// TEST DATA
-	messages := make([]map[string]string, 0)
-
-	message1 := map[string]string{
-		"id":      "1",
-		"message": "bonjour",
-	}
-
-	message2 := map[string]string{
-		"id":      "2",
-		"message": "AuRevoir",
-	}
-
-	messages = append(messages, message1)
-	messages = append(messages, message2)
+	// Initialization of the application ( Pusher Client )
 
 	// Let's configuration about Pusher
 	viper.SetConfigName("conf")
@@ -39,10 +27,7 @@ func main() {
 		panic(fmt.Errorf("Fatal error config file: %s", err))
 	}
 
-	fmt.Println(viper.GetString("pusherAppId"))
-	fmt.Println(viper.GetString("pusherKey"))
-	fmt.Println(viper.GetString("pusherSecret"))
-
+	// Config Pusher Application
 	client := pusher.Client{
 		AppId:   viper.GetString("pusherAppId"),
 		Key:     viper.GetString("pusherKey"),
@@ -51,37 +36,36 @@ func main() {
 		Secure:  true,
 	}
 
+	// Create Iris application
 	app := iris.New()
 
 	tmpl := iris.Handlebars("./templates", ".html")
 	tmpl.Reload(true)
 
+	// Add this parameter for the web assets file ( like css file / Js file )
 	app.RegisterView(tmpl)
 	app.StaticWeb("/templates", "./templates")
 
+	// Racine of the application display comments + form to add one
 	app.Get("/", func(ctx iris.Context) {
-		fmt.Println("New Connection")
-
 		ctx.ViewData("messages", messages)
 		ctx.View("index.html")
 	})
 
+	// When form submited, add the message to the messages variables.
 	app.Post("/message", func(ctx iris.Context) {
-		mess := message{}
-		err := ctx.ReadJSON(mess)
-		if err != nil {
-			ctx.StatusCode(iris.StatusInternalServerError)
+		var mess message
+		if err := ctx.ReadJSON(&mess); err != nil {
+			ctx.StatusCode(iris.StatusBadRequest)
 			ctx.WriteString(err.Error())
+			return
 		}
 
-		fmt.Println("New Post data")
-		newMessage := map[string]string{
-			"id":      mess.id,
-			"message": mess.body,
+		newMessage := map[string]interface{}{
+			"id":      mess.ID,
+			"message": mess.Body,
 		}
 		messages = append(messages, newMessage)
-
-		ctx.ViewData("messages", messages)
 
 		client.Trigger("message", "new_message", newMessage)
 	})
